0e01cc3b9841cc3a30f0725c4834220269363a9b
khchiang1121 <khchiang1121@gmail.com>
2025-08-06 20:52:56 +0800

    modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# Verify that Postgres is healthy before applying the migrations and running the Django development server
-# if [ "$DATABASE" = "postgres" ] then echo "Waiting for postgres..."
-#     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-#       sleep 0.1
-#     done
-#     echo "PostgreSQL started"
-# fi
+
 source .env
 
+# Verify that Postgres is healthy before applying the migrations and running the Django development server
 echo "Waiting for postgres at $POSTGRES_HOST:$POSTGRES_PORT..."
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    sleep 0.5
+# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#     sleep 0.5
+# done
+# echo "PostgreSQL started"
+
+# 等待 PostgreSQL 準備就緒
+until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+  echo "等待 PostgreSQL 啟動中..."
+  sleep 2
 done
+
 echo "PostgreSQL started"
 
 # 套用 migrations（不會重新產生 migration 檔案）
